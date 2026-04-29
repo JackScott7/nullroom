4bd1e681b80304b3366b697037e747cf4cbf85cc
JackScott7 <cloner.bl12@gmail.com>
2026-04-30 01:01:06 +0330

    minimal messaging platform

--- main.py
+++ main.py
@@ -1,16 +1,63 @@
-# This is a sample Python script.
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
+from lib.conn_manager import ContextManager
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = FastAPI(title="Nullroom")
+ws_manager = ContextManager()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Nullroom</title>
+    </head>
+    <body>
+        <h1>Nullroom Chat</h1>
+        <h2>Your ID: <span id="ws-id"></span></h2>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var client_id = Date.now()
+            document.querySelector("#ws-id").textContent = client_id;
+            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@app.get("/")
+async def root():
+    return HTMLResponse(html)
+
+
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(ws: WebSocket, client_id: str):
+    await ws_manager.connect(ws)
+    try:
+        while True:
+            data = await ws.receive_text()
+            await ws_manager.broadcast(ws, data)
+    except WebSocketDisconnect:
+        await ws_manager.disconnect(ws, client_id)
+
+
