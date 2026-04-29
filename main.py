00109bd67fff3a54ac92f1cb2390de61fc5fa202
JackScott7 <cloner.bl12@gmail.com>
2026-04-30 02:29:32 +0330

    feat: new endpoint implemented, /conns, /api/userAvailable and etc...

--- main.py
+++ main.py
@@ -1,63 +1,46 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import HTMLResponse
-from lib.conn_manager import ContextManager
+import os
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
+from fastapi.responses import HTMLResponse, JSONResponse
+from lib.conn_manager import ContextManager, NullUser
 
 app = FastAPI(title="Nullroom")
 ws_manager = ContextManager()
 
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Nullroom</title>
-    </head>
-    <body>
-        <h1>Nullroom Chat</h1>
-        <h2>Your ID: <span id="ws-id"></span></h2>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var client_id = Date.now()
-            document.querySelector("#ws-id").textContent = client_id;
-            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
-
-
 @app.get("/")
 async def root():
-    return HTMLResponse(html)
-
+    with open(os.path.join(os.path.dirname(__file__), "templates", "index.html")) as f:
+        return HTMLResponse(content=f.read(), status_code=200)
 
 
 @app.websocket("/ws/{client_id}")
 async def websocket_endpoint(ws: WebSocket, client_id: str):
-    await ws_manager.connect(ws)
+    user = NullUser(client_id, ws)
+    await ws_manager.connect(user)
     try:
         while True:
             data = await ws.receive_text()
-            await ws_manager.broadcast(ws, data)
+            await ws_manager.broadcast(data)
     except WebSocketDisconnect:
-        await ws_manager.disconnect(ws, client_id)
+        await ws_manager.disconnect(user)
+
+
+@app.post("/api/userAvailable")
+async def is_user_available(request: Request):
+    data = await request.json()
+    if not data:
+        return JSONResponse(status_code=400, content={"message": "No data provided"})
+
+    found = ws_manager.find_user(data.get('username', None))
+    if not found:
+        return JSONResponse(status_code=200, content={"status": "available"})
+    else:
+        return JSONResponse(status_code=409, content={"status": "taken"})
+
+
+@app.get("/conns")
+def get_conns():
+    return JSONResponse(status_code=200, content={"no_connections": len(ws_manager.active_connections)})
+
 
 
