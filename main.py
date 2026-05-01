e6e6d78d2e76e25b2cf1e07c6cdf954c6676531a
JackScott7 <cloner.bl12@gmail.com>
2026-05-01 17:37:54 +0330

    Add: Cors-Origin handling for localhost

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-import os
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
 from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from lib.conn_manager import ContextManager, NullUser
 
+
 app = FastAPI(title="Nullroom")
+origins = [
+    "http://localhost",
+    "http://localhost:5173"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"]
+)
 ws_manager = ContextManager()
 
 
-@app.get("/")
-async def root():
-    with open(os.path.join(os.path.dirname(__file__), "templates", "index.html")) as f:
-        return HTMLResponse(content=f.read(), status_code=200)
-
-
-@app.websocket("/ws/{client_id}")
+@app.websocket("/api/ws/{client_id}")
 async def websocket_endpoint(ws: WebSocket, client_id: str):
     user = NullUser(client_id, ws)
     await ws_manager.connect(user)
@@ -46,7 +51,7 @@ async def is_user_available(request: Request):
         return JSONResponse(status_code=409, content={"status": "taken"})
 
 
-@app.get("/conns")
+@app.get("/api/conns")
 def get_conns():
     return JSONResponse(status_code=200, content={"no_connections": len(ws_manager.active_connections)})
 
