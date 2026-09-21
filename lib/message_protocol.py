6e6d0c68c96cbcdb97168ebbecbba457b2d9eee1
Jack Scott <cloner.bl12@gmail.com>
2026-09-21 17:50:01 +0330

    add: new Protocols

--- lib/message_protocol.py
+++ lib/message_protocol.py
@@ -4,16 +4,21 @@ from enum import StrEnum
 class MessageProtocol(StrEnum):
     SET_COLOR = "set_color"
 
+    USER_LEFT = "user_left"
     USER_JOINED = "user_joined"
+    USER_COUNT = "user_count"
 
     JOIN_ROOM = "join_room"
     CREATE_ROOM = "create_room"
 
     ROOM_JOINED = "room_joined"
     ROOM_CREATED = "room_created"
+    ROOM_FULL = "room_full"
+    ROOM_CLOSED = "room_closed"
     LEAVE_ROOM = "leave_room"
 
     GET_PUBLIC_ROOMS = "get_public_rooms"
+    PUBLIC_ROOMS = "public_rooms"
     ROOM_NOT_FOUND = "room_not_found"
     NEW_ROOM_AVAILABLE = "new_room_available"
 
