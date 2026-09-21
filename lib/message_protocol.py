4cafcb2a7e682e3636d0ea1e901ad32cefd35a25
Jack Scott <cloner.bl12@gmail.com>
2026-09-21 17:42:16 +0330

    add: organized and added new MessageProtocols

--- lib/message_protocol.py
+++ lib/message_protocol.py
@@ -2,12 +2,22 @@ from enum import StrEnum
 
 
 class MessageProtocol(StrEnum):
+    SET_COLOR = "set_color"
+
+    USER_JOINED = "user_joined"
+
     JOIN_ROOM = "join_room"
     CREATE_ROOM = "create_room"
-    SEND_CHAT_MESSAGE = "send_chat_message"
-    LEAVE_ROOM = "leave_room"
+
     ROOM_JOINED = "room_joined"
-    ROOM_NOT_FOUND = "room_not_found"
-    SET_COLOR = "set_color"
+    ROOM_CREATED = "room_created"
+    LEAVE_ROOM = "leave_room"
+
     GET_PUBLIC_ROOMS = "get_public_rooms"
+    ROOM_NOT_FOUND = "room_not_found"
+    NEW_ROOM_AVAILABLE = "new_room_available"
+
+    SEND_CHAT_MESSAGE = "send_chat_message"
+    MESSAGE_SENT = "message_sent"
+
     GET_ONLINE_USERS_COUNT = "get_online_users_count"
